6940edcfe8b5c41737af9e716eb971022059e69c
duodenim <>
2020-04-18 15:35:46 -0500

    Initial impl of user island tracking

--- bot.js
+++ bot.js
@@ -2,9 +2,12 @@ const jsdom = require('jsdom');
 const fetch = require('node-fetch');
 const fs = require('fs');
 const Discord = require('discord.js');
-const client = new Discord.Client();
 const config = require('./config.json');
+const sqlite3 = require('sqlite3');
+const sqlite = require('sqlite');
 const {JSDOM} = jsdom;
+const { open } = sqlite;
+const client = new Discord.Client();
 
 const base_url = 'https://nooksisland.com';
 const design_url = base_url + '/designs?search=';
@@ -13,6 +16,9 @@ const prefix = config.prefix;
 const server_settings = config.server_settings;
 
 const cache_imgs = true;
+
+let database = undefined;
+let commands_in_progress = new Map();
 
 async function searchPattern(query) {
     const response = await fetch(design_url + query);
@@ -78,30 +84,147 @@ function checkConfig() {
     return !failed;
 }
 
+async function initDB(db) {
+    await db.exec('CREATE TABLE users (user_id TINYTEXT, player_name TINYTEXT, island_name TINYTEXT, code TINYTEXT)');
+    const result = await db.get('SELECT * FROM users');
+}
+
+async function newUserPrompts(message) {
+    const user = message.author;
+    const user_id = message.author.id.toString();
+
+    //Database not initialized yet, do nothing
+    if (database == undefined) {
+        return;
+    }
+
+    const curr_user_data = await database.get('SELECT * FROM users WHERE user_id = (:id)', {
+        ':id': user_id
+    });
+    if (curr_user_data == undefined) {
+        await database.run('INSERT INTO users(user_id) VALUES (:id)', {
+            ':id': user_id
+        });
+        commands_in_progress.set(user.id, 'newuser');
+        user.send('What is your character\'s name?');
+    } else if (curr_user_data.player_name == null) {
+        const name = message.content;
+        console.log(name);
+        await database.run('UPDATE users SET player_name = (:name) WHERE user_id = (:id)', {
+            ':name': name,
+            ':id': user_id
+        });
+        user.send('What is your island name?');
+    } else if (curr_user_data.island_name == null) {
+        const name = message.content;
+        console.log(name);
+        await database.run('UPDATE users SET island_name = (:name) WHERE user_id = (:id)', {
+            ':name': name,
+            ':id': user_id
+        });
+        user.send(`Great, ${curr_user_data.player_name}! You\'re all set up! Use ${prefix}open DODO_CODE to open your island`);
+        commands_in_progress.delete(user.id);
+    } else {
+        user.send("You're already set up!");
+    }
+}
+
 client.on('ready', () => {
     console.log('Logged in');
 });
 
 client.on('message', (msg) => {
-    if (isAllowed(msg) && msg.content.startsWith(`${prefix}search`)) {
-        let search_words = msg.content.split(' ');
-        if (search_words.length > 1) {
-            search_words.shift();
-            let search_txt = search_words.join('+');
-            searchPattern(search_txt).then( (urls) => {
-                if (urls.length > 0) {
-                    msg.channel.send({files: urls}).catch(console.error);
+    if (isAllowed(msg)) {
+        const command = (msg.content.startsWith(`${prefix}`)) ? msg.content.split(' ')[0].replace(`${prefix}`, '') : commands_in_progress.get(msg.author.id);
+        let params = msg.content.split(' ');
+        params.shift();
+        switch(command) {
+            case 'search': {
+                if (params.length > 0) {
+                    let search_txt = params.join('+');
+                    searchPattern(search_txt).then( (urls) => {
+                        if (urls.length > 0) {
+                            msg.channel.send({files: urls}).catch(console.error);
+                        } else {
+                            msg.channel.send(`No matches for ${params.join(' ')} :(`);
+                        } 
+                    });
                 } else {
-                    msg.channel.send(`No matches for ${search_words.join(' ')} :(`);
-                } 
-            });
-        } else {
-            msg.channel.send(`Usage: ${prefix}search keyword`);
+                    msg.channel.send(`Usage: ${prefix}search keyword`);
+                }
+                break;
+            }
+            case 'newuser': {
+                newUserPrompts(msg);
+                break;
+            }
+            case 'open': {
+                if ( params.length == 1 ) {
+                    const code = params[0];
+                    const user_id = msg.author.id.toString();
+                    database.get('SELECT * FROM users WHERE user_id = ?', user_id).then((user_data) => {
+                        if (user_data == undefined) {
+                            msg.author.send(`Please set up your island with ${prefix}newuser first!`);
+                        } else {
+                            console.log(user_data);
+                            msg.channel.send(`${user_data.player_name}\'s Island (${user_data.island_name}) is open at ${code}`);
+                            database.run('UPDATE users SET code = (:code) WHERE user_id = (:id)', {
+                                ':code': code,
+                                ':id': user_data.user_id
+                            });
+                        }
+                    });
+                    
+                } else {
+                    msg.channel.send(`Usage: ${prefix}post DODO_CODE`);
+                }
+                break;
+            }
+            case 'close': {
+                const user_id = msg.author.id.toString();
+                database.get('SELECT * FROM users WHERE user_id = ?', user_id).then((user_data) =>{
+                    if (user_data == undefined) {
+                        msg.author.send(`Please set up your island with ${prefix}newuser first!`);
+                    } else {
+                        if (user_data.code) {
+                            msg.channel.send(`Closed ${user_data.player_name}\'s Island`);
+                            database.run("UPDATE users SET code = '' WHERE user_id = ?", user_id);
+                        }
+                    }
+                });
+                break;
+            }
+            case 'islands': {
+                database.all("SELECT * FROM users WHERE (code IS NOT NULL AND code != '')").then((users) => {
+                    console.log(users);
+                    if (users.length > 0) {
+                        let txt = 'The following islands are open:\n';
+                        users.forEach((user) => {
+                            txt += `${user.player_name}\'s Island: ${user.island_name} at ${user.code}\n`;
+                        });
+                        msg.channel.send(txt);
+                    } else {
+                        msg.channel.send('No islands are open :(');
+                    }
+                });
+                break;
+            }
         }
     }
 });
 
 if (checkConfig()) {
+    open({ 
+        filename: 'database.db',
+        //filename: ':memory:',
+        driver: sqlite3.Database
+    }).then((db) => {
+        console.log(db);
+        initDB(db).then(() => {
+            console.log('database initialized');
+            database = db;
+        });
+    });
     client.login(token);
 } else {
     process.exit(0);
